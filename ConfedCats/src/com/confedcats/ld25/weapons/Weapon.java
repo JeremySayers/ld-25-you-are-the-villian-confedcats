4480956a2304232144ca1b8f8863ab07f92327a1
bosrsf04 <bosrsf04@gmail.com>
2012-12-15 17:37:22 +0000

    Added: Weapon and Ammo Skeleton

---
+++ ConfedCats/src/com/confedcats/ld25/weapons/Weapon.java
@@ -0,0 +1,60 @@
+package com.confedcats.ld25.weapons;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import com.confedcats.ld25.weapons.ammo.Ammo;
+
+public abstract class Weapon extends BufferedImage {
+	private int health;
+	private int x;
+	private int y;
+	private ArrayList<Ammo> ammoFired = new ArrayList<Ammo>();
+	private static final String PREFIX = "/com/confedcats/ld25/weapons/";
+	public Weapon(BufferedImage img, int health) {
+		super(img.getWidth(), img.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		Graphics g = getGraphics();
+		g.drawImage(img, 0, 0, null);
+		this.setHealth(health);
+	}
+	public static BufferedImage loadImage(String fname) {
+		try {
+			return ImageIO.read(Weapon.class.getResource(PREFIX+fname).openStream());
+		} catch (Exception e) {
+		}
+		return null;
+	}
+	public abstract Ammo getAmmo();
+	public Rectangle getBounds() {
+		return new Rectangle(getX(), getY(), getWidth(), getHeight());
+	}
+	public abstract int getFullHealth();
+	public int getHealth() {
+		return health;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void paint(Graphics g) {
+		update();
+		g.drawImage(this, getX(), getY(), null);
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public abstract void shoot();
+	public abstract void update();
+}
