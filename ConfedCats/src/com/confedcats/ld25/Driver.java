ac7a23ed2c9af8d916def2d0f344d4795f07e28f
bosrsf04 <bosrsf04@gmail.com>
2012-12-15 23:40:45 +0000

    Added: Toggle Fullscreen using Joystick

--- ConfedCats/src/com/confedcats/ld25/Driver.java
+++ ConfedCats/src/com/confedcats/ld25/Driver.java
@@ -9,6 +9,9 @@ import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
 
+import net.java.games.input.Controller;
+
+import com.confedcats.ld25.jInput.JInputJoystick;
 import com.confedcats.ld25.sounds.Sound;
 
 public class Driver extends Applet {
@@ -18,6 +21,9 @@ public class Driver extends Applet {
 	public static final GamePanel PANEL = new GamePanel();
 	public static final JFrame POPOUT = new JFrame("Confederate Cats");
 	private boolean muted = false;
+	// First you need to create controller.
+	public static final JInputJoystick joystick = new JInputJoystick(Controller.Type.GAMEPAD);
+	// Check if the controller was found.
 	public void init() {
 		// Initialize Applet
 		setSize(WIDTH, HEIGHT);
@@ -34,23 +40,40 @@ public class Driver extends Applet {
 		PANEL.addKeyListener(new KeyAdapter(){
 			public void keyReleased(KeyEvent event) {
 				if (event.getKeyCode()==KeyEvent.VK_F) {
-					if (POPOUT.isVisible()) {
-						add(PANEL, BorderLayout.CENTER);
-						PANEL.setSize(new Dimension(WIDTH,HEIGHT));
-						POPOUT.setVisible(false);
-						PANEL.requestFocus();
-						DEVICE.setFullScreenWindow(null); // Un-Associate popout with full screen.
-					} else {
-						POPOUT.add(PANEL, BorderLayout.CENTER);
-						PANEL.setPreferredSize(new Dimension(1,1));
-						POPOUT.setVisible(true);
-						PANEL.requestFocus();
-						DEVICE.setFullScreenWindow(POPOUT); // Re-Associate popout with full screen.
-					}
+					setFullscreen(!POPOUT.isVisible());
 				}
 			}
 		});
 		/* End Of Full Screen Pop Out Class */
+		
+		if(!joystick.isControllerConnected()){
+			   System.out.println("No controller found!");
+		}
+		new java.util.Timer().schedule(new java.util.TimerTask(){
+			public void run() {
+				if(joystick.isControllerConnected()){
+					joystick.pollController();
+					if (joystick.getButtonValue(0)){
+						setFullscreen(!POPOUT.isVisible());
+					}
+				}
+			}
+		}, 1, 10);
+	}
+	public void setFullscreen(boolean fulls) {
+		if (fulls) {
+			POPOUT.add(PANEL, BorderLayout.CENTER);
+            PANEL.setPreferredSize(new Dimension(1,1));
+            POPOUT.setVisible(true);
+            PANEL.requestFocus();
+            DEVICE.setFullScreenWindow(POPOUT); // Re-Associate popout with full screen.
+		} else {
+			add(PANEL, BorderLayout.CENTER);
+			PANEL.setSize(new Dimension(WIDTH,HEIGHT));
+			POPOUT.setVisible(false);
+			PANEL.requestFocus();
+			DEVICE.setFullScreenWindow(null); // Un-Associate popout with full screen.
+		}
 	}
 	public void start() {
 		Sound.setMute(muted);
