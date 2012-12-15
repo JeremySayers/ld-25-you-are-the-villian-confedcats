4958e4449f81d50edd87896bfba5a2adb6ac67d5
Jeremy1080 <Jeremy1080@gmail.com>
2012-12-15 07:26:05 +0000

    Bug Fix: Fixed going from fullscreen to normal size.

--- ConfedCats/src/com/confedcats/ld25/Driver.java
+++ ConfedCats/src/com/confedcats/ld25/Driver.java
@@ -1,6 +1,7 @@
 package com.confedcats.ld25;
 import java.applet.Applet;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
 import java.awt.event.KeyAdapter;
@@ -29,13 +30,15 @@ public class Driver extends Applet {
 		POPOUT.setUndecorated(true);
 		PANEL.addKeyListener(new KeyAdapter(){
 			public void keyReleased(KeyEvent event) {
-				if (event.getKeyCode()==KeyEvent.VK_F11) {
+				if (event.getKeyCode()==KeyEvent.VK_F) {
 					if (POPOUT.isVisible()) {
 						add(PANEL, BorderLayout.CENTER);
+						PANEL.setSize(new Dimension(WIDTH,HEIGHT));
 						POPOUT.setVisible(false);
 						DEVICE.setFullScreenWindow(null); // Un-Associate popout with full screen.
 					} else {
 						POPOUT.add(PANEL, BorderLayout.CENTER);
+						PANEL.setPreferredSize(new Dimension(1,1));
 						POPOUT.setVisible(true);
 						DEVICE.setFullScreenWindow(POPOUT); // Re-Associate popout with full screen.
 					}
