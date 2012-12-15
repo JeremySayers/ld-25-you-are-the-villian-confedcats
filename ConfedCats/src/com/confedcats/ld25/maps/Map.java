85630a38f307130419a481a09e4cb50ff382fce0
bosrsf04 <bosrsf04@gmail.com>
2012-12-15 07:39:26 +0000

    Added: Map int-to-Image Converter and Rainbow Map

--- ConfedCats/src/com/confedcats/ld25/maps/Map.java
+++ ConfedCats/src/com/confedcats/ld25/maps/Map.java
@@ -1,45 +1,56 @@
 package com.confedcats.ld25.maps;
 
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.BufferedImage;
 import java.util.HashMap;
 
 import com.confedcats.ld25.enemies.Boss;
 import com.confedcats.ld25.enemies.Enemy;
 
 public abstract class Map {
-	private String name;
 	private Image[][] tiles;
-	private Image background;
-	private int difficulty;
-	private Enemy[] validEnemies;
-	private HashMap<Integer, Boss> bosses; // Level:Boss
-	public Map(String name, Image background, int difficulty,
-			Enemy[] validEnemies, HashMap<Integer, Boss> bosses) {
+	public Map() {
 		super();
-		this.name = name;
-		this.background = background;
-		this.difficulty = difficulty;
-		this.validEnemies = validEnemies;
-		this.bosses = bosses;
-		this.tiles = generateMap();
+		this.tiles = convertMap();
 	}
-	public abstract Image[][] generateMap();
-	public Image getBackground() {
-		return background;
+	public Image[][] convertMap() {
+		int[][] intMap = generateMap();
+		Image[] tiles = getMapTiles();
+		Image[][] map;
+		if (intMap.length>0) {
+			map = new Image[intMap.length][intMap[1].length];
+			for (int y=0; y<intMap.length; y++) {
+				for (int x=0; x<intMap[y].length; x++) {
+					try {
+						map[y][x] = tiles[intMap[y][x]];
+					} catch (Exception e) {
+						map[y][x] = new BufferedImage(40, 40, BufferedImage.TYPE_INT_ARGB);
+					}
+				}
+			}
+		} else {
+			return new Image[0][0];
+		}
+		return map;
 	}
-	public HashMap<Integer, Boss> getBosses() {
-		return bosses;
-	}
-	public int getDifficulty() {
-		return difficulty;
-	}
-	public String getName() {
-		return name;
-	}
+	public abstract int[][] generateMap();
+	public abstract Image getBackground();
+	public abstract HashMap<Integer, Boss> getBosses();
+	public abstract int getDifficulty();
+	public abstract Image[] getMapTiles();
+	public abstract String getName();
 	public Image[][] getTiles() {
 		return tiles;
 	}
-	public Enemy[] getValidEnemies() {
-		return validEnemies;
+	public abstract Enemy[] getValidEnemies();
+	public void paint(Graphics g) {
+		g.drawImage(getBackground(), 0, 0, null);
+		Image[][] tiles = getTiles();
+		for (int y=0; y<tiles.length; y++) {
+			for (int x=0; x<tiles[y].length; x++) {
+				g.drawImage(tiles[y][x], x*40, y*40, (x+1)*40, (y+1)*40, null);
+			}
+		}
 	}
 }
---
+++ ConfedCats/src/com/confedcats/ld25/maps/Rainbow.java
@@ -0,0 +1,68 @@
+package com.confedcats.ld25.maps;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+
+import com.confedcats.ld25.Driver;
+import com.confedcats.ld25.enemies.Boss;
+import com.confedcats.ld25.enemies.Enemy;
+
+public class Rainbow extends Map {
+	/* 
+	 * 0 - Empty
+	 * 1 - Left
+	 * 2 - Center
+	 * 3 - Right
+	 * 4 - Pit
+	 * 5 - Spout
+	 */
+	public static final int[][] MAP_MASK = new int[][]{
+		{2,2,2,2,2,2,2,2,2,5,5,2,2,2,2,2,2,2,2,2},
+		{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,1,2,2,2,2,2,2,2,2,2,2,3,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+		{2,0,1,2,3,0,0,1,2,2,2,2,3,0,0,1,2,3,0,2},
+		{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+		{2,0,1,2,3,0,1,2,3,0,1,2,3,0,1,2,3,0,0,2},
+		{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+		{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+		{2,2,2,2,3,0,0,0,1,2,3,0,0,0,1,2,2,2,2,2},
+		{2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2},
+		{2,2,2,2,2,2,2,2,2,4,4,2,2,2,2,2,2,2,2,2}};
+	public int[][] generateMap() {
+		return MAP_MASK;
+	}
+	public Image getBackground() {
+		BufferedImage img = new BufferedImage(Driver.WIDTH, Driver.HEIGHT, BufferedImage.TYPE_INT_RGB);
+		Graphics g = img.getGraphics();
+		g.setColor(Color.PINK);
+		g.fillRect(0, 0, img.getWidth(), img.getHeight());
+		return img;
+	}
+
+	public HashMap<Integer, Boss> getBosses() {
+		return new HashMap<Integer, Boss>();
+	}
+
+	public int getDifficulty() {
+		return 0;
+	}
+
+	public String getName() {
+		return "Rainbow";
+	}
+
+	public Enemy[] getValidEnemies() {
+		return new Enemy[]{};
+	}
+	public Image[] getMapTiles() {
+		return new Image[]{new BufferedImage(40, 40, BufferedImage.TYPE_INT_ARGB)};
+	}
+
+}
