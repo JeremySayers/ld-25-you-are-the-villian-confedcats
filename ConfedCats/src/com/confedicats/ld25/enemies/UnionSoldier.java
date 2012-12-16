c69ccbbf8e3afc3ca9d57f4baa188491d21082f1
bosrsf04 <bosrsf04@gmail.com>
2012-12-16 06:58:14 +0000

    Added: Angry Union Solider and spawning based on spout locations

--- ConfedCats/src/com/confedicats/ld25/enemies/UnionSoldier.java
+++ ConfedCats/src/com/confedicats/ld25/enemies/UnionSoldier.java
@@ -7,11 +7,11 @@ import javax.imageio.ImageIO;
 public class UnionSoldier extends Enemy {
 	private BufferedImage LEFT;
 	private BufferedImage RIGHT;
-	public UnionSoldier(int health, int x, int y, int multi){
+	public UnionSoldier(int health, int x, int y, int multi, boolean angry){
 		super(health, x, y,multi);
 		try {
-			LEFT = ImageIO.read(UnionSoldier.class.getResource("/com/confedicats/ld25/enemies/cc_enemy_basicl.png"));
-			RIGHT = ImageIO.read(UnionSoldier.class.getResource("/com/confedicats/ld25/enemies/cc_enemy_basicr.png"));
+			LEFT = ImageIO.read(UnionSoldier.class.getResource("/com/confedicats/ld25/enemies/cc_enemy_basicl"+(angry?"_angry":"")+".png"));
+			RIGHT = ImageIO.read(UnionSoldier.class.getResource("/com/confedicats/ld25/enemies/cc_enemy_basicr"+(angry?"_angry":"")+".png"));
 		} catch (Exception e){
 		}
 	}
